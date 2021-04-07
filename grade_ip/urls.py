c5e85dc49d0a7468ac85811f879baddec113a407
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-07 14:34:35 +0300

    update urls.py

--- grade_ip/urls.py
+++ grade_ip/urls.py
@@ -15,7 +15,22 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path, include
+from users import views as user_views
+from django.conf.urls.static import static
+from django.conf import settings
+from project import views as project_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', include('project.urls')),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
