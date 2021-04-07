3c3b4017a1a0ee48e877f1d4f9f7412419e48de4
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-07 12:38:42 +0300

    update models.py

--- project/models.py
+++ project/models.py
@@ -1,6 +1,16 @@
-# -*- coding: utf-8 -*-
+from django.utils import timezone
+from django.contrib.auth.models import User
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    project_title = models.CharField(max_length=100)
+    design = models.TextField()
+    userbility = models.TextField()
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.project_title  
