18b2fd82c7fb00dced6cbd1b83a563591c6f7234
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-07 13:37:39 +0300

    update project views

--- project/views.py
+++ project/views.py
@@ -1,6 +1,17 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from __future__ import unicode_literals
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'posts' : Post.objects.all()
+    }
+    return render(request, 'project/home.html', context)
+
+def about(request):
+    return render(request, 'project/about.html', {'title': 'About'})
+
+def info(request):
+    return render(request, 'project/info.html', {'title': 'Info'})
+
